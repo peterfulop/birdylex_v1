5ff603bf883f85d6fb22f1e51402fb5f93b187af
Peter <mrpeterfulop@gmail.com>
2021-11-19 20:28:02 +0100

    11.19. FileUpload process

--- API/services/dbService.js
+++ API/services/dbService.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 class DbService {
 
   sanitizeHtml = require("sanitize-html");
@@ -9,6 +12,74 @@ class DbService {
       ? false
       : true;
   }
+
+  setUserFolders = (userId) => {
+
+    const basePath = `./public/images/users/${userId}`;
+
+    if (!fs.existsSync(basePath)) {
+      fs.mkdirSync(basePath);
+    }
+    if (!fs.existsSync(basePath + "/avatar")) {
+      fs.mkdirSync(basePath + "/avatar");
+    }
+    if (!fs.existsSync(basePath + "/prev")) {
+      fs.mkdirSync(basePath + "/prev");
+    }
+    if (!fs.existsSync(basePath + "/puffer")) {
+      fs.mkdirSync(basePath + "/puffer");
+    }
+
+  }
+
+  clearPufferFolder = async (pufferPath) => {
+
+    fs.promises.readdir(pufferPath, (err, files) => {
+      if (err) throw err;
+      if (files.length > 0) {
+        for (const file of files) {
+          fs.unlink(path.join(pufferPath, file), err => {
+            if (err) throw err;
+          });
+        }
+      }
+    }).then(proc => {
+      console.log("then");
+      return {
+        stat: true,
+        proc
+      }
+
+    }).catch(err => {
+      return err;
+    });
+
+
+  }
+
+  setPufferedImage = async (file, pufferPath, fileExt) => {
+    file.mv(pufferPath + "/pufered" + fileExt, (err) => {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  }
+
+  clearPreviewFolder = async (previewPath) => {
+    fs.readdir(previewPath, async (err, files) => {
+      if (err) throw err;
+      if (files.length > 0) {
+        for (const file of files) {
+          fs.unlink(path.join(prevDir, file), err => {
+            if (err) throw err;
+          });
+        }
+      }
+    });
+  }
+
+
+
 }
 
 
